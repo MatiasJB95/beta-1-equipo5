80e0217cd10f39be5c1ffb3edd57a4c570b98e04
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-29 20:05:10 -0300

    Update resena.java

--- src/main/java/com/grupo5/resenalibro/persistence/entity/resena.java
+++ src/main/java/com/grupo5/resenalibro/persistence/entity/resena.java
@@ -1,14 +1,12 @@
 package com.grupo5.resenalibro.persistence.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.NotFound;
 
 import java.time.LocalDateTime;
 
@@ -25,5 +23,10 @@ public class resena {
     private Long usuarioId;
     private String contenido;
     private LocalDateTime fecha = LocalDateTime.now();
+
+    @Column(nullable = false)
+    @Min(1)
+    @Max(5)
+    private Integer puntuacion;
 
 }
