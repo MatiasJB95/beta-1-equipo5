8f870c8a9a2b00ea5927200b565a9f20ca85ba48
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-29 20:10:52 -0300

    Update resenaRepository.java

--- src/main/java/com/grupo5/resenalibro/persistence/repository/resenaRepository.java
+++ src/main/java/com/grupo5/resenalibro/persistence/repository/resenaRepository.java
@@ -1,4 +1,11 @@
 package com.grupo5.resenalibro.persistence.repository;
 
-public class resenaRepository {
+import com.grupo5.resenalibro.persistence.entity.Resena;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface resenaRepository extends JpaRepository<Resena, Long> {
+    List<Resena> findByUsuarioId(Long usuarioId);
+    List<Resena> findByExternalBookId(String externalBookId);
 }
