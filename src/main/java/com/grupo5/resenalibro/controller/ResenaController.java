52c6f90824262416494e5e3ffe149a1c425e7b46
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-29 20:24:34 -0300

    Update ResenaController.java

--- src/main/java/com/grupo5/resenalibro/controller/ResenaController.java
+++ src/main/java/com/grupo5/resenalibro/controller/ResenaController.java
@@ -1,6 +1,7 @@
 package com.grupo5.resenalibro.controller;
 
 import com.grupo5.resenalibro.dto.ResenaRequest;
+import com.grupo5.resenalibro.dto.ResenaResponse;
 import com.grupo5.resenalibro.persistence.entity.Resena;
 import com.grupo5.resenalibro.service.ResenaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,14 +19,19 @@ import java.util.List;
         private ResenaService resenaService;
 
         @PostMapping
-        public ResponseEntity<Resena> crearReseña(@RequestBody ResenaRequest request) {
+        public ResponseEntity<ResenaResponse> crearReseña(@RequestBody ResenaRequest request) {
+            Resena nueva = resenaService.crearResena(request);
             return ResponseEntity.status(HttpStatus.CREATED)
-                    .body(resenaService.crearReseña(request));
+                    .body(new ResenaResponse(nueva));
+
         }
 
         @GetMapping("/usuario/{usuarioId}")
-        public List<Resena> getByUsuario(@PathVariable Long usuarioId) {
-            return resenaService.getByUsuario(usuarioId);
+        public List<ResenaResponse> getByUsuario(@PathVariable Long usuarioId) {
+            return resenaService.getByUsuario(usuarioId)
+                    .stream()
+                    .map(ResenaResponse::new)
+                    .toList();
         }
 
         @GetMapping("/libro/{externalBookId}")
