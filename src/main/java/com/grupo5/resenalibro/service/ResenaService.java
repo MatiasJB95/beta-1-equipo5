00472a312e02a8c08bcad9b0f18958b45571fe91
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-29 20:13:17 -0300

    Update ResenaService.java

--- src/main/java/com/grupo5/resenalibro/service/ResenaService.java
+++ src/main/java/com/grupo5/resenalibro/service/ResenaService.java
@@ -1,7 +1,34 @@
 package com.grupo5.resenalibro.service;
 
+import com.grupo5.resenalibro.dto.ResenaRequest;
+import com.grupo5.resenalibro.persistence.entity.Resena;
+import com.grupo5.resenalibro.persistence.repository.resenaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ResenaService {
+    @Autowired
+    private resenaRepository resenaRepo;
+
+    public Resena crearReseña(ResenaRequest request) {
+        Resena resena = new Resena();
+        resena.setExternalBookId(request.externalBookId);
+        resena.setUsuarioId(request.usuarioId);
+        resena.setContenido(request.contenido);
+        resena.setFecha(LocalDateTime.now());
+
+        return resenaRepo.save(resena);
+    }
+
+    public List<Resena> getByUsuario(Long usuarioId) {
+        return resenaRepo.findByUsuarioId(usuarioId);
+    }
+
+    public List<Resena> getByLibro(String externalBookId) {
+        return resenaRepo.findByExternalBookId(externalBookId);
+    }
 }
