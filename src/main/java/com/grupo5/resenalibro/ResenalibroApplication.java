c7b861adc2785f94c1d391c36a02acd4bde99d36
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-29 18:53:31 -0300

    Create

---
+++ src/main/java/com/grupo5/resenalibro/ResenalibroApplication.java
@@ -0,0 +1,13 @@
+package com.grupo5.resenalibro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResenalibroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResenalibroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo5/resenalibro/ResenalibroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo5.resenalibro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResenalibroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
