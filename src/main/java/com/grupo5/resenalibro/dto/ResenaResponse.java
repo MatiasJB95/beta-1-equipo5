ebbcb4b54f78b47078dfd7305948b7560208df10
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-29 20:24:38 -0300

    Create ResenaResponse.java

---
+++ src/main/java/com/grupo5/resenalibro/dto/ResenaResponse.java
@@ -0,0 +1,21 @@
+package com.grupo5.resenalibro.dto;
+
+import com.grupo5.resenalibro.persistence.entity.Resena;
+
+import java.time.LocalDateTime;
+
+public class ResenaResponse {
+    public String externalBookId;
+    public Long usuarioId;
+    public String contenido;
+    public Integer puntuacion;
+    public LocalDateTime fecha;
+
+    public ResenaResponse(Resena r) {
+        this.externalBookId = r.getExternalBookId();
+        this.usuarioId = r.getUsuarioId();
+        this.contenido = r.getContenido();
+        this.puntuacion = r.getPuntuacion();
+        this.fecha = r.getFecha();
+    }
+}
