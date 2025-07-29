7e28fe388eec6853cc5a74cc7a3f45584d31e3a7
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-29 19:50:09 -0300

    Update

--- src/main/java/com/grupo5/resenalibro/persistence/entity/resena.java
+++ src/main/java/com/grupo5/resenalibro/persistence/entity/resena.java
@@ -1,5 +1,29 @@
 package com.grupo5.resenalibro.persistence.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NotFound;
+
+import java.time.LocalDateTime;
+
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class resena {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String externalBookId;
+    private Long usuarioId;
+    private String;
+    private LocalDateTime fecha = LocalDateTime.now();
+
 }
