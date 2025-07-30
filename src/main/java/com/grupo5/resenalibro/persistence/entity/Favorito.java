8539a2fa73a317dc6cb7a64eeb3bba45b347cbfa
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-30 20:35:34 -0300

    Update Favorito.java

--- src/main/java/com/grupo5/resenalibro/persistence/entity/Favorito.java
+++ src/main/java/com/grupo5/resenalibro/persistence/entity/Favorito.java
@@ -1,5 +1,68 @@
 package com.grupo5.resenalibro.persistence.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Favorito {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Long usuarioId;
+
+    @NotBlank
+    private String externalId;
+
+    @NotBlank
+    private String tipo; // "B" o "M"
+
+    private LocalDateTime fechaAgregado = LocalDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getExternalId() {
+        return externalId;
+    }
+
+    public void setExternalId(String externalId) {
+        this.externalId = externalId;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDateTime getFechaAgregado() {
+        return fechaAgregado;
+    }
+
+    public void setFechaAgregado(LocalDateTime fechaAgregado) {
+        this.fechaAgregado = fechaAgregado;
+    }
 }
