dc60212adfe97a8b2a5c810cb5fe971e9ba9b893
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-30 18:54:46 -0300

    Update ResenaRequest.java

--- src/main/java/com/grupo5/resenalibro/dto/ResenaRequest.java
+++ src/main/java/com/grupo5/resenalibro/dto/ResenaRequest.java
@@ -3,7 +3,7 @@ package com.grupo5.resenalibro.dto;
 import java.time.LocalDateTime;
 
 public class ResenaRequest {
-    public String externalBookId;
+    public String externalId;
     public Long usuarioId;
     public String contenido;
     public LocalDateTime fecha;
