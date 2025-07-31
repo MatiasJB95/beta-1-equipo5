defdade54f4ae73f930e7e96b43982bf8943fb3c
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-31 16:23:36 -0300

    Update ResenaResponse.java

--- src/main/java/com/grupo5/resenalibro/dto/ResenaResponse.java
+++ src/main/java/com/grupo5/resenalibro/dto/ResenaResponse.java
@@ -5,6 +5,7 @@ import com.grupo5.resenalibro.persistence.entity.Resena;
 import java.time.LocalDateTime;
 
 public class ResenaResponse {
+    public Long Id;
     public String externalId;
     public Long usuarioId;
     public String contenido;
@@ -12,6 +13,7 @@ public class ResenaResponse {
     public LocalDateTime fecha;
 
     public ResenaResponse(Resena r) {
+        this.Id = r.getId();
         this.externalId = r.getExternalId();
         this.usuarioId = r.getUsuarioId();
         this.contenido = r.getContenido();
