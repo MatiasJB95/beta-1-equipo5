6a28fcbf69fd8d99e5e064a67723c85f42bd5d27
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-31 00:20:25 -0300

    Update ResenaRequest.java

--- src/main/java/com/grupo5/resenalibro/dto/ResenaRequest.java
+++ src/main/java/com/grupo5/resenalibro/dto/ResenaRequest.java
@@ -1,10 +1,5 @@
 package com.grupo5.resenalibro.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-
 import java.time.LocalDateTime;
 
 public class ResenaRequest {
@@ -12,12 +7,6 @@ public class ResenaRequest {
     public Long usuarioId;
     public String contenido;
     public LocalDateTime fecha;
-    @NotNull
-    @Min(1)
-    @Max(5)
     public Integer puntuacion;
-
-    @NotNull
-    @Pattern(regexp = "[BM]", message = "El tipo debe ser B o M")
-    private String tipo;
+    public String tipo;
 }
