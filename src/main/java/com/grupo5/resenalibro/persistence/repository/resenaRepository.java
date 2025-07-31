836c9e7153c63b370f63aba8880629882d82ef3d
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-31 00:20:32 -0300

    Update resenaRepository.java

--- src/main/java/com/grupo5/resenalibro/persistence/repository/resenaRepository.java
+++ src/main/java/com/grupo5/resenalibro/persistence/repository/resenaRepository.java
@@ -2,16 +2,28 @@ package com.grupo5.resenalibro.persistence.repository;
 
 import com.grupo5.resenalibro.persistence.entity.Resena;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface resenaRepository extends JpaRepository<Resena, Long> {
     List<Resena> findByUsuarioId(Long usuarioId);
 
-    List<Resena> findByExternalBookId(String externalBookId);
+    List<Resena> findByExternalId(String externalBookId);
 
     Optional<Resena> findById(Long id);
     List<Resena> findByUsuarioIdAndTipo(Long usuarioId, String tipo);
+
+    @Query("""
+    SELECT
+        SUM(CASE WHEN r.tipo = 'B' THEN 1 ELSE 0 END),
+        SUM(CASE WHEN r.tipo = 'M' THEN 1 ELSE 0 END)
+    FROM Resena r
+    WHERE r.usuarioId = :usuarioId
+""")
+    List<Object[]> contarPorTipo(@Param("usuarioId") Long usuarioId);
 
 }
