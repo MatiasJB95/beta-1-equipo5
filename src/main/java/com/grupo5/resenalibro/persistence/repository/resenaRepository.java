49dbaf268f5e5c9930b1c3ba7d6d081a2eb4b4a7
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-30 22:40:09 -0300

    Update resenaRepository.java

--- src/main/java/com/grupo5/resenalibro/persistence/repository/resenaRepository.java
+++ src/main/java/com/grupo5/resenalibro/persistence/repository/resenaRepository.java
@@ -13,4 +13,5 @@ public interface resenaRepository extends JpaRepository<Resena, Long> {
 
     Optional<Resena> findById(Long id);
     List<Resena> findByUsuarioIdAndTipo(Long usuarioId, String tipo);
+
 }
