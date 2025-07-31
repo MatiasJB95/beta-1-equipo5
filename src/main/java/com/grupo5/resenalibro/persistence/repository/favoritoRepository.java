739054ca66c9422b221dc5a97f3d060c4f81764b
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-07-31 00:20:31 -0300

    Update favoritoRepository.java

--- src/main/java/com/grupo5/resenalibro/persistence/repository/favoritoRepository.java
+++ src/main/java/com/grupo5/resenalibro/persistence/repository/favoritoRepository.java
@@ -2,10 +2,11 @@ package com.grupo5.resenalibro.persistence.repository;
 
 import com.grupo5.resenalibro.persistence.entity.Favorito;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface favoritoRepository extends JpaRepository<Favorito, Long> {
     List<Favorito> findByUsuarioIdAndTipo(Long usuarioId, String tipo);
     Optional<Favorito> findByUsuarioIdAndExternalIdAndTipo(Long usuarioId, String externalId, String tipo);
